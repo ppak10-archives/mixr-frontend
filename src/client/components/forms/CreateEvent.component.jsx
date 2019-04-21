0e47bf09f0efa6fd282fe7f897b9d904b5099a0a
Peter <ppak10@gmail.com>
2019-04-20 21:39:42 -0400

    Create Event Form
    Summary: Adds some of the form input components for create event form
    
    Test Plan: local
    
    Reviewers: test
    
    Reviewed By: test
    
    Differential Revision: https://feedmeyourdata.com/D76

--- src/client/components/forms/CreateEvent.component.jsx
+++ src/client/components/forms/CreateEvent.component.jsx
@@ -1,13 +1,46 @@
 /**
  * CreateEvent.component.jsx
- * Form component for creating events
+ * Form component htmlFor creating events
  */
 
 // Node Modules
 import React from 'react';
 
-const CreateEventForm = (props) => {
-  return <div>Create Event Form</div>;
+const CreateEventForm = () => {
+  return (
+    <div className="create-event-form-wrapper">
+      <form>
+        <div className="form-group">
+          <label>Event Name</label>
+          <input
+            className="form-control"
+            placeholder="Networking Dinner, Lunch Meeting, etc."
+            type="text"
+          />
+        </div>
+        <div className="form-group">
+          <label>Event Description</label>
+          <textarea
+            className="form-control"
+            placeholder="Event to connect with others over good food"
+            rows="3"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="inputAddress">Address</label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputAddress"
+            placeholder="1234 Main St"
+          />
+        </div>
+        <button className="btn btn-primary btn-block" type="submit">
+          Create Event
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default CreateEventForm;
