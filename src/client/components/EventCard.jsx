561a0ef66ffc1621a874504f000de786c00bf301
Peter <ppak10@gmail.com>
2019-05-03 18:25:29 -0400

    NPM Package Updates
    Summary: Updates npm packages and uses react-map-elements 0.0.9
    
    Test Plan: local
    
    Reviewers: test
    
    Reviewed By: test
    
    Differential Revision: https://feedmeyourdata.com/D95

--- src/client/components/EventCard.jsx
+++ src/client/components/EventCard.jsx
@@ -6,30 +6,38 @@
 // Node Modules
 import moment from 'moment';
 import React from 'react';
-import {BaseMap} from 'react-map-elements';
+import {StaticMap} from 'react-map-elements';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 // Constants
 import {EVENT} from '../constants/proptypes';
 
-export const EventCard = ({event}) => (
-  <Card className="event-card">
-    <Card.Header className="header">
-      <BaseMap />
-    </Card.Header>
-    <Card.Body className="body">
-      <Card.Title className="title">{event.title}</Card.Title>
-      <Card.Text className="description">
-        {event.description || 'No Description'}
-      </Card.Text>
-    </Card.Body>
-    <ListGroup variant="flush">
-      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-      <ListGroup.Item>{moment(event.time_start).calendar()}</ListGroup.Item>
-    </ListGroup>
-  </Card>
-);
+export const EventCard = ({event}) => {
+  // Props
+  const coordinates = {
+    lat: event.lat,
+    lng: event.lng,
+  };
+
+  return (
+    <Card className="event-card">
+      <Card.Header className="header">
+        <StaticMap coordinates={coordinates} />
+      </Card.Header>
+      <Card.Body className="body">
+        <Card.Title className="title">{event.title}</Card.Title>
+        <Card.Text className="description">
+          {event.description || 'No Description'}
+        </Card.Text>
+      </Card.Body>
+      <ListGroup variant="flush">
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>{moment(event.time_start).calendar()}</ListGroup.Item>
+      </ListGroup>
+    </Card>
+  );
+};
 
 EventCard.propTypes = {
   event: EVENT,
